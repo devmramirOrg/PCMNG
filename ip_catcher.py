b159fa791bf80d3b546e7726c76700ec141013a4
Hosein Erish <theerilia@gmail.com>
2023-06-16 09:20:43 +0330

    removed

--- ip_catcher.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def get_router_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('8.8.8.8', 80))  # connect to Google DNS server
-    router_ip = s.getsockname()[0]
-    s.close()
-    return router_ip
-
